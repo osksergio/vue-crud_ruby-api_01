b9614464047f1545c9520f160ba5fe98f741e76c
Sergio Oseko <osk.sergio@gmail.com>
2024-08-26 22:58:23 -0300

    feat: botões e ações de insert, edit e delete.

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -3,12 +3,72 @@
     <h1>Posts</h1>
 
     <!-- Create or Post -->
-    <input type="text" v-model="title" placeholder="Title" class="title-input" />
-    <input type="text" v-model="body" placeholder="Body" class="body-input" />
+    <input type="text" 
+      v-model="title" 
+      placeholder="Title" 
+      class="title-input" />
+    <input type="text" 
+      v-model="body" 
+      placeholder="Body" 
+      class="body-input" />
+
+    <!-- only render if editing post -->
+    <button v-if="isEditing" @click="updatePost">Update</button>
+    <button v-if="isEditing" @click="cancelEdit">Cancel</button>    
+
+    <!-- only render if not editing post -->
+    <button v-else @click="createPost">Create</button>
+
+    <!-- list of posts -->
+    <div v-for="post in posts" :key="post.id">
+      <h5>[{{ post.id }}] {{ post.title }}</h5>
+      <p>{{ post.body }}</p>
+
+      <button @click="editPost(post.id)">Edit</button>
+      <button @click="deletePost(post.id)">Delete</button>
+    </div>
+
   </div>
 </template>
 
 <script setup>
+  import { ref, onMounted } from 'vue';
+
+  const posts = ref([])
+
+  const title = ref('')
+  const body = ref('')
+  const post_id = ref(0)
+  const isEditing = ref(false)
+  const API_URL = "http://localhost:3000/posts"
+
+  const createPost = async() => {
+    const res = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'   
+      },
+      body: JSON.stringify({
+        title: title.value,
+        body: body.value
+      })
+    })
+
+    const data = await res.json()
+
+    posts.value.push(data)
+    title.value = ''
+    body.value = ''
+    post_id.value = 0;
+  }
+
+  const updatePost = async() => {
+    return true;
+  }
+
+  const cancelEdit = () => {
+    return true;
+  }
 </script>
 
 <style scoped>
